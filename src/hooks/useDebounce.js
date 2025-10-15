8d75e3bdfe6beff42de68e3756dded2453b9d9a1
Anwesha Palit <apalit@redhat.com>
2025-10-15 13:11:05 +0530

    fix:check pac

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -7,6 +7,9 @@ export function useDebounce(value, delay) {
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
   }, [value, delay]);
 
   return debouncedValue;
